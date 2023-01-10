d7ddd9a82648ace0790d39792ed3f6e806903fba
claybowl <claybowlproductions@gmail.com>
2023-01-09 20:46:00 -0800

    task 10

---
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""module 10-ill_use_my_scale
+Calculates the shape of a numpy array.
+"""
+
+
+def np_shape(matrix):
+    """Returns the shape of a numpy array"""
+    return matrix.shape
