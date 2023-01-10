11d8ccfdc8485e2743e54a9578cc972a79028db3
claybowl <claybowlproductions@gmail.com>
2023-01-10 13:07:05 -0800

    task 12 again

--- math/0x00-linear_algebra/12-bracin_the_elements.py
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -7,12 +7,4 @@ multiplication, and division.
 
 def np_elementwise(mat1, mat2):
     """Returns the elementwise operations"""
-    element_sum = [[a + b for a, b in zip(row1, row2)]for row1, row2 in
-                   zip(mat1, mat2)]
-    element_sub = [[a - b for a, b in zip(row1, row2)]for row1, row2 in
-                   zip(mat1, mat2)]
-    element_mul = [[a * b for a, b in zip(row1, row2)]for row1, row2 in
-                   zip(mat1, mat2)]
-    element_div = [[a / b for a, b in zip(row1, row2)]for row1, row2 in
-                   zip(mat1, mat2)]
-    return (element_sum, element_sub, element_mul element_div)
+    return(mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
