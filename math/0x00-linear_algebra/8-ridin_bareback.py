91f70665c40d1582ed39692f3339a74246cf2505
claybowl <claybowlproductions@gmail.com>
2023-01-09 18:04:42 -0800

    task 8

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -7,5 +7,16 @@ import numpy as np
 
 def mat_mul(mat1, mat2):
     """matrix multiplication"""
-    product = np.dot(mat1, mat2)
-    return product
+    if len(mat1[0]) != len(mat2):
+        return
+    else:
+        product = []
+        for i in range(len(mat1)):
+            row = []
+            for j in range(len(mat2[0])):
+                element = 0
+                for k in range(len(mat1[0])):
+                    element += mat1[i][k] * mat2[k][j]
+                row.append(element)
+            product.append(row)
+        return product
