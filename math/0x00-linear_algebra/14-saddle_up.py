ecaff26ab8445c3c7bc0cd2b0ec1c1598ce30c77
claybowl <claybowlproductions@gmail.com>
2023-01-10 17:10:43 -0800

    task 13, task 14

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""module 14-saddle_up
+Function that performs matrix multiplication.
+"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """matrix multiplication"""
+    return np.matmul(mat1, mat2)
