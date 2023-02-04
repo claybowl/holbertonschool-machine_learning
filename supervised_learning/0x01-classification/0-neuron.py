e4f5c2fca4cccab36eaf88ab8e7e33e867bdac5e
claybowl <claybowlproductions@gmail.com>
2023-02-03 22:28:27 -0600

    fixed self.w

--- supervised_learning/0x01-classification/0-neuron.py
+++ supervised_learning/0x01-classification/0-neuron.py
@@ -14,6 +14,6 @@ class Neuron:
             raise TypeError("nx must be an integer")
         if nx <= 1:
             raise ValueError("nx must be a positive integer")
-        self.W = 0
+        self.W = np.random.normal(size=(1, nx))
         self.b = 0
         self.A = 0
