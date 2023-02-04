0df73071300c8f7434552821badeb86a1ef0db84
claybowl <claybowlproductions@gmail.com>
2023-02-04 06:00:32 -0600

    task 2 - sigmoid

---
+++ supervised_learning/0x01-classification/2-neuron.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""module 2-neuron
+Writes a class Neuron that defines a single neuron
+performing binary classification.
+"""
+import numpy as np
+
+
+class Neuron:
+    """class Neuron"""
+
+    def __init__(self, nx):
+        """initializes a neuron with the given number of inputs"""
+        if type(nx) != int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.normal(size=(1, nx))
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """getter function for W"""
+        return self.__W
+
+    @property
+    def b(self):
+        """getter function for b"""
+        return self.__b
+
+    @property
+    def A(self):
+        """getter function for A"""
+        return self.__A
+
+    def sigmoid_act(self, x):
+        """sigmoid activation function"""
+        return 1/(1 + np.exp(-x))
+
+    def forward_prop(self, X):
+        """calculates the forward propagation of the neuron"""
+        Z = np.matmul(self.W, X) + self.b
+        self.A = self.sigmoid_act(Z)
