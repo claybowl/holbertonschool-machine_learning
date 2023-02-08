a0e581f390fabf9106daa948aa8ee65e1e967bf0
claybowl <claybowlproductions@gmail.com>
2023-02-08 16:22:31 -0600

    testing..

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -6,11 +6,15 @@ import numpy as np
 
 
 def one_hot_encode(Y, classes):
+    """converts a numeric label vector into a one-hot matrix"""
     if type(Y) is not np.ndarray:
         return None
 
     m = Y.shape[0]
-    one_hot = np.zeros((classes, m))
-    one_hot[Y, np.arange(m)] = 1
+    try:
+        one_hot = np.zeros((classes, m))
+        one_hot[Y, np.arange(m)] = 1
 
-    return one_hot
+        return one_hot
+    except Exception:
+        return None
