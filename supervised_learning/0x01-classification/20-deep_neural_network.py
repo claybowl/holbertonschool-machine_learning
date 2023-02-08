60a896db11ab81df1a441865135b3aac9108bf0c
claybowl <claybowlproductions@gmail.com>
2023-02-08 12:41:27 -0600

    task 20

---
+++ supervised_learning/0x01-classification/20-deep_neural_network.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+"""module 19-deep_neural_network
+Class Deep Neural Network
+"""
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """Class Deep Neural Network"""
+
+    def __init__(self, nx, layers):
+        """initializes the neural network"""
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(layers) is not list or len(layers) == 0:
+            raise TypeError("layers must be a list of positive integers")
+
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+
+        for l in range(1, self.L + 1):
+            he_init = np.sqrt(2 / nx)
+            self.weights['W' +
+                         str(l)] = np.random.randn(layers[l - 1], nx) * he_init
+            self.weights['b' + str(l)] = np.zeros((layers[l - 1], 1))
+            nx = layers[l - 1]
+
+    def forward_prop(self, X):
+        """forward propagation"""
+        self.__cache["A0"] = X
+        for l in range(1, self.__L + 1):
+            W = self.__weights["W" + str(l)]
+            b = self.__weights["b" + str(l)]
+            A_prev = self.__cache["A" + str(l - 1)]
+            Z = np.dot(W, A_prev) + b
+            A = 1 / (1 + np.exp(-Z))
+            self.__cache["A" + str(l)] = A
+
+        return A, self.__cache
+
+    @property
+    def L(self):
+        """getter for the number of layers"""
+        return self.__L
+
+    @property
+    def cache(self):
+        """getter for the cache"""
+        return self.__cache
+
+    @property
+    def weights(self):
+        """getter for the weights"""
+        return self.__weights
+
+    def cost(self, Y, A):
+        """calculates the cost"""
+        m = Y.shape[1]
+        cost = -(1/m) * np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
+        return cost
+
+    def evaluate(self, X, Y):
+        """evaluates the model"""
+        cache = self.forward_prop(X)
+        A = cache["A" + str(self.__L)]
+        prediction = np.where(A >= 0.5, 1, 0)
+        cost = self.cost(Y, A)
+        return prediction, cost
