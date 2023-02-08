ca15057eccaecf38eb5102315a8d782587e5fc6a
claybowl <claybowlproductions@gmail.com>
2023-02-08 02:43:16 -0600

    reverting loop changes

--- supervised_learning/0x01-classification/16-deep_neural_network.py
+++ supervised_learning/0x01-classification/16-deep_neural_network.py
@@ -7,21 +7,23 @@ import numpy as np
 
 class DeepNeuralNetwork:
     def __init__(self, nx, layers):
-        if not isinstance(nx, int):
+        if type(nx) is not int:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if not isinstance(layers, list) or len(layers) == 0:
+        if type(layers) is not list or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
-        if any(not isinstance(i, int) or i <= 0 for i in layers):
-            raise TypeError("layers must be a list of positive integers")
+        for i in range(len(layers)):
+            if type(layers[i]) is not int or layers[i] < 1:
+                raise TypeError("layers must be a list of positive integers")
 
         self.L = len(layers)
         self.cache = {}
         self.weights = {}
+
         for l in range(1, self.L + 1):
-            w = np.random.randn(layers[l - 1], nx) * np.sqrt(2 / nx)
-            b = np.zeros((layers[l - 1], 1))
-            self.weights["W" + str(l)] = w
-            self.weights["b" + str(l)] = b
+            he_init = np.sqrt(2 / nx)
+            self.weights['W' +
+                         str(l)] = np.random.randn(layers[l - 1], nx) * he_init
+            self.weights['b' + str(l)] = np.zeros((layers[l - 1], 1))
             nx = layers[l - 1]
