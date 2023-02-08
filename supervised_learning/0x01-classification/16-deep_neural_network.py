f6e7a422267f0655bda27e3bae960de34d9b2c28
claybowl <claybowlproductions@gmail.com>
2023-02-08 02:39:50 -0600

    task 16 attempt 2

--- supervised_learning/0x01-classification/16-deep_neural_network.py
+++ supervised_learning/0x01-classification/16-deep_neural_network.py
@@ -11,22 +11,19 @@ class DeepNeuralNetwork:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if type(layers) is not list or not layers:
+        if type(layers) is not list or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
-        for layer in layers:
-            if type(layer) is not int or layer < 1:
+        for i in range(len(layers)):
+            if type(layers[i]) is not int or layers[i] < 1:
                 raise TypeError("layers must be a list of positive integers")
 
         self.L = len(layers)
         self.cache = {}
         self.weights = {}
 
-        for l in range(self.L):
-            if l == 0:
-                self.weights["W" + str(l + 1)
-                             ] = np.random.randn(layers[l], nx) * np.sqrt(2/nx)
-                self.weights["b" + str(l + 1)] = np.zeros((layers[l], 1))
-            else:
-                self.weights["W" + str(l + 1)] = np.random.randn(layers[l],
-                                                                 layers[l - 1]) * np.sqrt(2/layers[l - 1])
-                self.weights["b" + str(l + 1)] = np.zeros((layers[l], 1))
+        for l in range(1, self.L + 1):
+            he_init = np.sqrt(2 / nx)
+            self.weights['W' +
+                         str(l)] = np.random.randn(layers[l - 1], nx) * he_init
+            self.weights['b' + str(l)] = np.zeros((layers[l - 1], 1))
+            nx = layers[l - 1]
