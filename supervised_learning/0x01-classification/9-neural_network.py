8b0856f9a710b1ef44ee8347a101a8fa2e5065e9
claybowl <claybowlproductions@gmail.com>
2023-02-07 20:38:21 -0600

    task 9 attempt

--- supervised_learning/0x01-classification/9-neural_network.py
+++ supervised_learning/0x01-classification/9-neural_network.py
@@ -3,3 +3,48 @@
 Defines a neural network with one hidden layer performing binary classification
 """
 import numpy as np
+
+
+class NeuralNetwork:
+    """class NeuralNetwork"""
+
+    def __init__(self, nx, nodes):
+        if type(nx) is not int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(nodes) != int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        return self.__W1
+
+    @property
+    def b1(self):
+        return self.__b1
+
+    @property
+    def A1(self):
+        return self.__A1
+
+    @property
+    def W2(self):
+        return self.__W2
+
+    @property
+    def b2(self):
+        return self.__b2
+
+    @property
+    def A2(self):
+        return self.__A2
