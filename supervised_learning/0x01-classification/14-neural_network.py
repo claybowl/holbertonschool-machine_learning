fixing gradient descent

supervised_learning/0x01-classification/14-neural_network.py
@@ -79,11 +79,15 @@ class NeuralNetwork:
         """calculates one pass of gradient descent"""
         m = X.shape[1]
         dz2 = A2 - Y
-        dw2 = np.matmul(A1, dz2.T) / m
+        dw2 = np.matmul(dz2, A1.T) / m
         db2 = np.sum(dz2, axis=1, keepdims=True) / m
         dz1 = np.matmul(self.__W2.T, dz2) * (A1 * (1 - A1))
         dw1 = np.matmul(dz1, X.T) / m
-        db1 = np.sum
+        db1 = np.sum(dz1, axis=1, keepdims=True) / m
+        self.__W1 -= alpha * dw1
+        self.__b1 -= alpha * db1
+        self.__W2 -= alpha * dw2
+        self.__b2 -= alpha * db2
 
     def train(self, X, Y, iterations=5000, alpha=0.05):
         """trains the neural network"""