89ece74c9583dce923120cbeacfba4f6449c3b42
Clayton Christian <103296303+claybowl@users.noreply.github.com>
2023-02-09 11:12:15 -0600

    fixed gradient function

--- supervised_learning/0x01-classification/13-neural_network.py
+++ supervised_learning/0x01-classification/13-neural_network.py
@@ -83,4 +83,8 @@ class NeuralNetwork:
         db2 = np.sum(dz2, axis=1, keepdims=True) / m
         dz1 = np.matmul(self.__W2.T, dz2) * (A1 * (1 - A1))
         dw1 = np.matmul(dz1, X.T) / m
-        db1 = np.sum
+        db1 = np.sum(dz1, axis=1, keepdims=True) / m
+        self.__W1 -= alpha * dw1
+        self.__b1 -= alpha * db1
+        self.__W2 -= alpha * dw2
+        self.__b2 -= alpha * db2
