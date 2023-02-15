d457e2e2901802d000e6054394d8c93949af2503
claybowl <claybowlproductions@gmail.com>
2023-02-15 04:02:07 -0600

    redundent feed_dict

--- supervised_learning/0x00-tensorflow/6-train.py
+++ supervised_learning/0x00-tensorflow/6-train.py
@@ -59,8 +59,6 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha, i
                 print("\tValidation Cost: {}".format(valid_cost))
                 print("\tValidation Accuracy: {}".format(valid_accuracy))
 
-            sess.run(train_op, feed_dict={x: X_train, y: Y_train})
-
         saver = tf.train.Saver()
         saver.save(sess, save_path)
     return save_path
