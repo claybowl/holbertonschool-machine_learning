31f02ffc648a83807e67e071040b0bf4d5cdac8c
claybowl <claybowlproductions@gmail.com>
2023-02-15 00:52:55 -0600

    pycode error

--- supervised_learning/0x00-tensorflow/5-create_train_op.py
+++ supervised_learning/0x00-tensorflow/5-create_train_op.py
@@ -7,5 +7,5 @@ import tensorflow as tf
 
 def create_train_op(loss, alpha):
     """ creates the training operation for the network"""
-    optimizer =  tf.train.GradientDescentOptimizer(learning_rate=alpha)
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate=alpha)
     return optimizer.minimize(loss)
