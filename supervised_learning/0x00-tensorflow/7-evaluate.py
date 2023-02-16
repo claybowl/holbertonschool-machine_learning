40129ebe62a1b9ff0662636eb819591caf6b0c04
claybowl <claybowlproductions@gmail.com>
2023-02-15 20:00:04 -0600

    task 7 attempt

--- supervised_learning/0x00-tensorflow/7-evaluate.py
+++ supervised_learning/0x00-tensorflow/7-evaluate.py
@@ -6,31 +6,26 @@ import tensorflow as tf
 
 
 def evaluate(X, Y, save_path):
-    """evaluates the output of a neural network"""
-    # Import modules
-    create_placeholders = __import__(
-        '0-create_placeholders').create_placeholders
-    forward_prop = __import__('2-forward_prop').forward_prop
-    create_train_op = __import__('5-create_train_op').create_train_op
-    calculate_loss = __import__('4-calculate_loss').calculate_loss
-    calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
-    layer_sizes, activations = create_placeholders(X.shape[1], Y.shape[1])
+    # Create a tensorflow session
+    sess = tf.Session()
 
-    # Evaluation of output
-    x, y = create_placeholders(X.shape[1], Y.shape[1])
-    y_pred = forward_prop(x, layer_sizes, activations)
-    accuracy = calculate_accuracy(y, y_pred)
-    loss = calculate_loss(y, y_pred)
+    # Restore the saved model
+    saver = tf.train.import_meta_graph(save_path + '.meta')
+    saver.restore(sess, save_path)
 
-    # Engage Tensorflow session
-    with tf.Session() as sess:
-        # Restore saved model
-        saver = tf.train.Saver()
-        saver.restore(sess, save_path)
+    # Get the tensors from the saved model
+    x = tf.get_collection('x')[0]
+    y = tf.get_collection('y')[0]
+    y_pred = tf.get_collection('y_pred')[0]
+    accuracy = tf.get_collection('accuracy')[0]
+    loss = tf.get_collection('loss')[0]
 
-        # Evaluate model
-        feed_dict = {x: X, y: Y}
-        y_pred_hot, accuracy_val, loss_val = sess.run(
-            [y_pred, accuracy, loss], feed_dict=feed_dict)
+    # Evaluate the model
+    feed_dict = {x: X, y: Y}
+    y_pred_eval, accuracy_eval, loss_eval = sess.run(
+        [y_pred, accuracy, loss], feed_dict=feed_dict)
 
-    return y_pred_hot, accuracy_val, loss_val
+    # Close the session
+    sess.close()
+
+    return y_pred_eval, accuracy_eval, loss_eval
