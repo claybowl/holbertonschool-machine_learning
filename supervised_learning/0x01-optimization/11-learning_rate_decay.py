c1ee1920d4c8c6935b23c2c21162d136e225ff5c
claybowl <claybowlproductions@gmail.com>
2023-02-22 11:00:24 -0600

    task 11 attempt

--- supervised_learning/0x01-optimization/11-learning_rate_decay.py
+++ supervised_learning/0x01-optimization/11-learning_rate_decay.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 """module 11-learning_rate_decay
-Updates the learning rate using inverse
-time decay in numpy
+A Python module that updates the learning rate 
+using inverse time decay in numpy.
+`alpha` - the initial learning rate
+`decay_rate` - the learning rate decay rate
+`global_step` - the current step of training
+`decay_step` - the step after which the learning rate should decay
 """
 
 
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
-    """Updates the learning rate using inverse time decay in numpy"""
+    """takes the learning rate parameters and returns
+    the current learning rate"""
     return alpha / (1 + decay_rate * (global_step // decay_step))
