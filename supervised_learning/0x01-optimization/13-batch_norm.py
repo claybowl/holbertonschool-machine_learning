dcef0e2c1d42293dfa620e62a134636221e4b1c4
claybowl <claybowlproductions@gmail.com>
2023-02-22 11:56:21 -0600

    task 13 pycodestyle

--- supervised_learning/0x01-optimization/13-batch_norm.py
+++ supervised_learning/0x01-optimization/13-batch_norm.py
@@ -8,10 +8,10 @@ import numpy as np
 
 def batch_norm(Z, gamma, beta, epsilon):
     """implements the tensorflow with the 'staircase' parameter
-    **Z** : un-activated output of a neural network
-    **gamma** : parameter for scaling
-    **beta** : parameter for shifting
-    **epsilon** : small number for numerical stabilization
+    'Z' : un-activated output of a neural network
+    'gamma' : parameter for scaling
+    'beta' : parameter for shifting
+    'epsilon' : small number for numerical stabilization
     """
     mean = np.mean(Z, axis=0)
     var = np.var(Z, axis=0)
