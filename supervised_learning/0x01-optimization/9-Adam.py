6f20c61c086755a0a1098f6852556830258ca0d8
claybowl <claybowlproductions@gmail.com>
2023-02-22 09:29:22 -0600

    pycodestyle

--- supervised_learning/0x01-optimization/9-Adam.py
+++ supervised_learning/0x01-optimization/9-Adam.py
@@ -7,10 +7,10 @@ import numpy as np
 
 
 def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
-	"""updates a variable in place using the Adam optimization algorithm"""
-	v = beta1 * v + (1 - beta1) * grad
-	s = beta2 * s + (1 - beta2) * (grad ** 2)
-	vdw_corrected = v / (1 - beta1 ** t)
-	sdw_corrected = s / (1 - beta2 ** t)
-	var = var - alpha * vdw_corrected / (sdw_corrected ** 0.5 + epsilon)
-	return var, v, s
+    """updates a variable in place using the Adam optimization algorithm"""
+    v = beta1 * v + (1 - beta1) * grad
+    s = beta2 * s + (1 - beta2) * (grad ** 2)
+    vdw_corrected = v / (1 - beta1 ** t)
+    sdw_corrected = s / (1 - beta2 ** t)
+    var = var - alpha * vdw_corrected / (sdw_corrected ** 0.5 + epsilon)
+    return var, v, s
