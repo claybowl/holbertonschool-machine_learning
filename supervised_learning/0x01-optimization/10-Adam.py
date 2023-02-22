e3cbd5369dba01c918fc088cb9b4f45be17a6d9b
claybowl <claybowlproductions@gmail.com>
2023-02-22 10:46:48 -0600

    task 10 attempt

--- supervised_learning/0x01-optimization/10-Adam.py
+++ supervised_learning/0x01-optimization/10-Adam.py
@@ -9,7 +9,7 @@ import tensorflow as tf
 def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
     """training operation using Adam optimization algorithm"""
     # Creation of variables
-    t = tf.Variable(0, dtype=tf.int32)
+    t = tf.Variable(0, dtype=tf.float32)
     v = tf.Variable(tf.zeros_like(loss), dtype=tf.float32)
     s = tf.Variable(tf.zeros_like(loss), dtype=tf.float32)
 
@@ -23,7 +23,7 @@ def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
     # Updating 'v' and 's' and incrementing 't' by 1
     update_v = v.assign(beta1 * v + (1 - beta1) * grad)
     update_s = s.assign(beta2 * s + (1 - beta2) * grad**2)
-    t = t.assign_add(1)
+    Adam_op = tf.assign_add(t, 1)
 
     # Calculation of Adam's optimization
     alpha_t = alpha * tf.sqrt(1 - beta2**t) / (1 - beta1**t)
