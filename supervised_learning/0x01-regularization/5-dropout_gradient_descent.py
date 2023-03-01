a22bf4bf94f12d24a0519bf580e9aa647c49e4cc
claybowl <claybowlproductions@gmail.com>
2023-03-01 16:19:47 -0600

    changed variable

--- supervised_learning/0x01-regularization/5-dropout_gradient_descent.py
+++ supervised_learning/0x01-regularization/5-dropout_gradient_descent.py
@@ -24,16 +24,16 @@ import numpy as np
 def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
     """updates the weights of a neural network with Dropout regularization"""
     m = Y.shape[1]
-    for l in range(L, 0, -1):
+    for i in range(L, 0, -1):
         A_cur_layer = cache['A' + str(l)]
-        if l == L:
+        if i == L:
             dZ = A_cur_layer - Y
         else:
-            dZ = np.dot(weights['W' + str(l + 1)].T, dZ) * \
+            dZ = np.dot(weights['W' + str(i + 1)].T, dZ) * \
                 (1 - np.power(A_cur_layer, 2))
-            dZ = dZ * cache['D' + str(l)]
+            dZ = dZ * cache['D' + str(i)]
             dZ = dZ / keep_prob
-        dW = np.dot(dZ, cache['A' + str(l - 1)].T) / m
+        dW = np.dot(dZ, cache['A' + str(i - 1)].T) / m
         db = np.sum(dZ, axis=1, keepdims=True) / m
-        weights['W' + str(l)] = weights['W' + str(l)] - alpha * dW
-        weights['b' + str(l)] = weights['b' + str(l)] - alpha * db
+        weights['W' + str(i)] = weights['W' + str(i)] - alpha * dW
+        weights['b' + str(i)] = weights['b' + str(i)] - alpha * db
