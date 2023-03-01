8a67d71b7c5ebc3cead0abf1077b29848fb3b635
claybowl <claybowlproductions@gmail.com>
2023-03-01 16:31:31 -0600

    pycode

--- supervised_learning/0x01-regularization/6-dropout_create_layer.py
+++ supervised_learning/0x01-regularization/6-dropout_create_layer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """module 6-dropout_create_layer.py
-Write a function def dropout_create_layer(prev, n, activation, keep_prob): that creates a layer of a neural network using dropout:
+Write a function
+def dropout_create_layer(prev, n, activation, keep_prob):
+that creates a layer of a neural network using dropout:
 
 prev is a tensor containing the output of the previous layer
 n is the number of nodes the new layer should contain
@@ -16,6 +18,7 @@ def dropout_create_layer(prev, n, activation, keep_prob):
     init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
     regularizer = tf.contrib.layers.l2_regularizer(scale=0.1)
     dense = tf.layers.dense(prev, n, activation=activation,
-                            kernel_initializer=init, kernel_regularizer=regularizer)
+                            kernel_initializer=init,
+                            kernel_regularizer=regularizer)
     dropout = tf.layers.dropout(dense, rate=1 - keep_prob)
     return dropout
