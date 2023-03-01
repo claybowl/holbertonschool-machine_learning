b91c914ecd837579eacbe5d3db5bde1e650ba7c8
claybowl <claybowlproductions@gmail.com>
2023-03-01 13:51:01 -0600

    task 2 attempt

--- supervised_learning/0x01-regularization/2-l2_reg_cost.py
+++ supervised_learning/0x01-regularization/2-l2_reg_cost.py
@@ -13,12 +13,12 @@ import tensorflow as tf
 
 def l2_reg_cost(cost, weights, lambtha, L):
     """calculates the cost of a neural network with L2 regularization"""
-    l2_reg = 0
+    l2_regularization_cost = 0
     for l in range(1, L + 1):
-        l2_reg += tf.reduce_sum(
+        l2_regularization_cost += tf.reduce_sum(
             tf.square(weights['W' + str(l)]))
 
-    l2_reg = lambtha / (2 * m) * l2_reg
-    cost = cost + l2_reg
+    l2_regularization_cost = lambtha / (2 * m) * l2_regularization_cost
+    cost = cost + l2_regularization_cost
 
     return cost
