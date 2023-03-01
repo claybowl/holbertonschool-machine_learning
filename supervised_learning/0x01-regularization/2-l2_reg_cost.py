53bf78874038d4dfcd2a1e8883f301ce6d3a42a6
claybowl <claybowlproductions@gmail.com>
2023-03-01 13:10:29 -0600

    task 2

---
+++ supervised_learning/0x01-regularization/2-l2_reg_cost.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""module 2-l2_reg_cost.py
+Write the function def l2_reg_cost(cost): that
+calculates the cost of a neural network with L2 regularization:
+
+cost is a tensor containing the cost of the
+network without L2 regularization
+Returns: a tensor containing the cost of the
+network accounting for L2 regularization
+"""
+import numpy as np
+import tensorflow as tf
+
+
+def l2_reg_cost(cost, weights, lambtha, L):
+    """calculates the cost of a neural network with L2 regularization"""
+    l2_reg = 0
+    for l in range(1, L + 1):
+        l2_reg += tf.reduce_sum(
+            tf.square(weights['W' + str(l)]))
+
+    l2_reg = lambtha / (2 * m) * l2_reg
+    cost = cost + l2_reg
+
+    return cost
