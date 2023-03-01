07a90f3d395dcc05a617e866f44e2e3b9c2333d6
claybowl <claybowlproductions@gmail.com>
2023-03-01 06:41:15 -0600

    pycodestyle

--- supervised_learning/0x00-error_analysis/4-f1_score.py
+++ supervised_learning/0x00-error_analysis/4-f1_score.py
@@ -7,7 +7,8 @@ confusion is a confusion numpy.ndarray of shape (classes, classes)
 where row indices represent the correct labels and column
 indices represent the predicted labels
 classes is the number of classes
-Returns: a numpy.ndarray of shape (classes,) containing the F1 score of each class
+Returns: a numpy.ndarray of shape (classes,)
+containing the F1 score of each class
 You must use sensitivity = __import__('1-sensitivity').sensitivity
 and precision = __import__('2-precision').precision create previously
 """
