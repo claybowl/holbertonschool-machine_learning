337d6bc5ce0e9f48d2913cbae047cb61fcdb7cf1
claybowl <claybowlproductions@gmail.com>
2023-03-01 14:11:48 -0600

    pycode

--- supervised_learning/0x01-regularization/3-l2_reg_create_layer.py
+++ supervised_learning/0x01-regularization/3-l2_reg_create_layer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """module 3-l2_reg_create_layer.py
 Write a function def l2_reg_create_layer
-(prev, n, activation, lambtha): that creates a tensorflow layer that includes L2 regularization:
+(prev, n, activation, lambtha): that creates a
+tensorflow layer that includes L2 regularization:
 
 prev is a tensor containing the output of the previous layer
 n is the number of nodes the new layer should contain
