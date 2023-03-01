baa98f2e077e9f3556e663aef8c08cd52a517f90
claybowl <claybowlproductions@gmail.com>
2023-03-01 06:19:16 -0600

    task 3 attempt

--- supervised_learning/0x00-error_analysis/3-specificity.py
+++ supervised_learning/0x00-error_analysis/3-specificity.py
@@ -4,11 +4,23 @@ Write the function def specificity(confusion): that calculates the specificity f
 
 confusion is a confusion numpy.ndarray of shape (classes, classes) where row indices represent the correct labels and column indices represent the predicted labels
 classes is the number of classes
-Returns: a numpy.ndarray of shape (classes,) containing the specificity of each class
+Returns: a numpy.ndarray of shape (classes,) containing the specificity of each classu
 """
 import numpy as np
 
 
 def specificity(confusion):
     """calculates the specificity for each class in a confusion matrix"""
-    
+    classes = confusion.shape[0]
+    specificity = np.zeros(classes)
+
+    # loop through each class
+    for i in range(classes):
+        # Calculate True Negative (tn) and False Positive (fp)
+        tn = np.delete(confusion, i, 0).sum() - \
+            np.delete(confusion, i, 1)[:, i].sum()
+        fp = confusion[:, i].sum() - confusion[i, i]
+        # Calculate specificity for the current class
+        specificity[i] = tn / (tn + fp)
+
+    return specificity
