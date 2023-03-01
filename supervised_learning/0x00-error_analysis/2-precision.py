87adc02956d4a407ffe012984069004f1b780cbe
claybowl <claybowlproductions@gmail.com>
2023-03-01 02:38:56 -0600

    task 2 attempt

---
+++ supervised_learning/0x00-error_analysis/2-precision.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""module 2-precision.py
+Write the function def precision(confusion): that calculates
+the precision for each class in a confusion matrix:
+
+confusion is a confusion numpy.ndarray of shape (classes, classes)
+where row indices represent the correct labels and column indices
+represent the predicted labels
+classes is the number of classes
+Returns: a numpy.ndarray of shape (classes,) containing the
+precision of each class
+"""
+import numpy as np
+
+
+def precision(confusion):
+    """calculates the precision for each class in a confusion matrix"""
+    classes = confusion.shape[0]
+    precision = np.zeros((classes,))
+
+    # Calculate precision for each class
+    for i in range(classes):
+        # Precision is calculated as the ratio of true positive to the sum of true positive and false positive
+        true_positive = confusion[i][i]
+        false_positive = np.sum(confusion[:, i]) - true_positive
+        precision[i] = true_positive / (true_positive + false_positive)
+
+    return precision
