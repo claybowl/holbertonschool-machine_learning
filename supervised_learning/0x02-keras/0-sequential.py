0dc9bb7ff9f7d8d889ab1be538e5a6ad0eb0ef5e
claybowl <claybowlproductions@gmail.com>
2023-03-07 10:31:48 -0600

    task 0 attempt

---
+++ supervised_learning/0x02-keras/0-sequential.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" 0x02. Keras """
+import keras
+from keras import regularizers
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """
+    Builds a neural network with the Keras library
+    """
+    model = keras.Sequential()
+    model.add(keras.layers.Dense(layers[0], activation=activations[0], input_shape=(nx,),
+                                 kernel_regularizer=regularizers.l2(lambtha)))
+    for i in range(1, len(layers)):
+        model.add(keras.layers.Dense(layers[i], activation=activations[i],
+                                     kernel_regularizer=regularizers.l2(lambtha)))
+        if i < len(layers) - 1:
+            model.add(keras.layers.Dropout(1 - keep_prob))
+    return model
