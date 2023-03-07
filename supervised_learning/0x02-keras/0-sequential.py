9b3b19d3534abc1e0ee863bfc05e06f6f18d5bc8
claybowl <claybowlproductions@gmail.com>
2023-03-07 10:34:19 -0600

    playing with imports

--- supervised_learning/0x02-keras/0-sequential.py
+++ supervised_learning/0x02-keras/0-sequential.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
 """ 0x02. Keras """
-import keras
+import tensorflow.keras as k
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
-    """
-    Builds a neural network with the Keras library
-    """
-    model = keras.Sequential()
-    model.add(keras.layers.Dense(layers[0], activation=activations[0], input_shape=(nx,),
-                                 kernel_regularizer=regularizers.l2(lambtha)))
-    for i in range(1, len(layers)):
-        model.add(keras.layers.Dense(layers[i], activation=activations[i],
-                                     kernel_regularizer=regularizers.l2(lambtha)))
+    """Builds a neural network with the Keras library"""
+    model = k.models.Sequential()
+    for i in range(len(layers)):
+        if i == 0:
+            model.add(k.layers.Dense(layers[i], activation=activations[i],
+                                     input_shape=(nx,),
+                                     kernel_regularizer=k.regularizers.l2(lambtha)))
+        else:
+            model.add(k.layers.Dense(layers[i], activation=activations[i],
+                                     kernel_regularizer=k.regularizers.l2(lambtha)))
         if i < len(layers) - 1:
-            model.add(keras.layers.Dropout(1 - keep_prob))
+            model.add(k.layers.Dropout(1 - keep_prob))
     return model
