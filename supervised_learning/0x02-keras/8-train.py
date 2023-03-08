204d393be331ad7e2da32e488a65d592464888f4
claybowl <claybowlproductions@gmail.com>
2023-03-07 20:47:58 -0600

    task 8 debug

--- supervised_learning/0x02-keras/8-train.py
+++ supervised_learning/0x02-keras/8-train.py
@@ -10,34 +10,25 @@ def train_model(network, data, labels, batch_size, epochs,
                 verbose=True, shuffle=False):
     """train_model: Trains a model using mini-batch gradient descent."""
     if validation_data:
-        validation_data_ = (validation_data[0], validation_data[1])
+        early_stopping_callback = None
+        if early_stopping:
+            early_stopping_callback = K.callbacks.EarlyStopping(
+                monitor='val_loss', patience=patience, restore_best_weights=True)
+        learning_rate_decay_callback = None
+        if learning_rate_decay:
+            def schedule(epoch):
+                return alpha / (1 + decay_rate * epoch)
+            learning_rate_decay_callback = K.callbacks.LearningRateScheduler(
+                schedule, verbose=1)
+        save_best_callback = None
+        if save_best:
+            save_best_callback = K.callbacks.ModelCheckpoint(
+                filepath, monitor='val_loss', save_best_only=True, save_weights_only=False, mode='min', verbose=1)
+        callbacks = [early_stopping_callback,
+                     learning_rate_decay_callback, save_best_callback]
+        history = network.fit(data, labels, batch_size=batch_size, epochs=epochs, verbose=verbose,
+                              shuffle=shuffle, validation_data=validation_data, callbacks=callbacks)
     else:
-        validation_data_ = None
-
-    if early_stopping and validation_data:
-        early_stopping_callback = K.callbacks.EarlyStopping(
-            monitor='val_loss', patience=patience)
-        callbacks = [early_stopping_callback]
-    else:
-        callbacks = []
-
-    if learning_rate_decay and validation_data:
-        def learning_rate_scheduler(epoch):
-            return alpha / (1 + decay_rate * epoch)
-
-        learning_rate_callback = K.callbacks.LearningRateScheduler(
-            learning_rate_scheduler, verbose=1)
-        callbacks.append(learning_rate_callback)
-
-    if save_best and validation_data:
-        best_model_callback = K.callbacks.ModelCheckpoint(
-            filepath, save_best_only=True,
-            monitor='val_loss', mode='min')
-        callbacks.append(best_model_callback)
-
-    history = network.fit(data, labels, batch_size=batch_size,
-                          epochs=epochs, verbose=verbose, shuffle=shuffle,
-                          validation_data=validation_data_,
-                          callbacks=callbacks)
-
+        history = network.fit(data, labels, batch_size=batch_size,
+                              epochs=epochs, verbose=verbose, shuffle=shuffle)
     return history
