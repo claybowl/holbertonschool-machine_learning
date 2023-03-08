c7a9c69c9395831790839b484cd8421795f66d76
claybowl <claybowlproductions@gmail.com>
2023-03-07 21:15:24 -0600

    pycodestyle

--- supervised_learning/0x02-keras/10-weights.py
+++ supervised_learning/0x02-keras/10-weights.py
@@ -5,12 +5,12 @@ import tensorflow.keras as k
 
 def save_weights(network, filename, save_format='h5'):
     """Saves a model's weights.
-    
+
     Arguments:
     network -- the model whose weights should be saved
     filename -- the path of the file that the weights should be saved to
     save_format -- the format in which the weights should be saved
-    
+
     Returns:
     None
     """
@@ -19,11 +19,11 @@ def save_weights(network, filename, save_format='h5'):
 
 def load_weights(network, filename):
     """Loads a model's weights.
-    
+
     Arguments:
     network -- the model to which the weights should be loaded
     filename -- the path of the file that the weights should be loaded from
-    
+
     Returns:
     None
     """
