1100b88ebdd40261f2b6526e52886fc0c16e298b
claybowl <claybowlproductions@gmail.com>
2023-03-07 21:03:24 -0600

    task 10 attempt

---
+++ supervised_learning/0x02-keras/10-weights.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+""" 0x02. Keras """
+import tensorflow.keras as k
+
+
+def save_weights(network, filename, save_format='h5'):
+    """Saves a model's weights.
+    
+    Arguments:
+    network -- the model whose weights should be saved
+    filename -- the path of the file that the weights should be saved to
+    save_format -- the format in which the weights should be saved
+    
+    Returns:
+    None
+    """
+    network.save_weights(filename, save_format=save_format)
+
+
+def load_weights(network, filename):
+    """Loads a model's weights.
+    
+    Arguments:
+    network -- the model to which the weights should be loaded
+    filename -- the path of the file that the weights should be loaded from
+    
+    Returns:
+    None
+    """
+    network.load_weights(filename)
