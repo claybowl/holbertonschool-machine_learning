bda4f8b69b2c20c1d47dc8730df32f5fe1930db1
claybowl <claybowlproductions@gmail.com>
2023-03-07 22:57:37 -0600

    pycode

--- supervised_learning/0x02-keras/12-test.py
+++ supervised_learning/0x02-keras/12-test.py
@@ -4,6 +4,7 @@ import tensorflow.keras as k
 
 
 def test_model(network, data, labels, verbose=True):
+    """Evaluates the output of a neural network."""
     # Evaluate the model on the test data
     results = network.evaluate(data, labels, verbose=verbose)
 
