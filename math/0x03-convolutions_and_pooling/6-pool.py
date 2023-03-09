5ff196e640cf3e9a241378e9d6e94b830f29003a
claybowl <claybowlproductions@gmail.com>
2023-03-08 23:45:49 -0600

    pycodestyle

--- math/0x03-convolutions_and_pooling/6-pool.py
+++ math/0x03-convolutions_and_pooling/6-pool.py
@@ -5,25 +5,25 @@ import numpy as np
 
 
 def pool(images, kernel_shape, stride, mode='max'):
-   """Performs pooling on images."""
-   m, h, w, c = images.shape
-   kh, kw = kernel_shape
-   sh, sw = stride
+    """Performs pooling on images."""
+    m, h, w, c = images.shape
+    kh, kw = kernel_shape
+    sh, sw = stride
 
-   # Determine output shape
-   out_h = int((h - kh) / sh) + 1
-   out_w = int((w - kw) / sw) + 1
+    # Determine output shape
+    out_h = int((h - kh) / sh) + 1
+    out_w = int((w - kw) / sw) + 1
 
-   # Initialize output array
-   out = np.zeros((m, out_h, out_w, c))
+    # Initialize output array
+    out = np.zeros((m, out_h, out_w, c))
 
-   # Perform pooling
-   for i in range(out_h):
-       for j in range(out_w):
-           img = images[:, i*sh:i*sh+kh, j*sw:j*sw+kw, :]
-           if mode == 'max':
-               out[:, i, j, :] = np.max(img, axis=(1, 2))
-           elif mode == 'avg':
-               out[:, i, j, :] = np.mean(img, axis=(1, 2))
+    # Perform pooling
+    for i in range(out_h):
+        for j in range(out_w):
+            img = images[:, i*sh:i*sh+kh, j*sw:j*sw+kw, :]
+            if mode == 'max':
+                out[:, i, j, :] = np.max(img, axis=(1, 2))
+            elif mode == 'avg':
+                out[:, i, j, :] = np.mean(img, axis=(1, 2))
 
-   return out
+    return out
