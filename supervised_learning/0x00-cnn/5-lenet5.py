72a4cc87137f7da354d41f53cc55967019e37ddc
claybowl <claybowlproductions@gmail.com>
2023-03-15 21:25:49 -0500

    task 5 attempt

---
+++ supervised_learning/0x00-cnn/5-lenet5.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+""" LeNet-5 (Tensorflow) """
+import tensorflow.keras as K
+
+
+def lenet5(X):
+    """Function that builds a modified version of the LeNet-5"""
+    # Convolutional layer 1
+    conv1 = K.layers.Conv2D(filters=6, kernel_size=(
+        5, 5), padding='same', activation='relu',
+        kernel_initializer='he_normal')(X)
+
+    # Max pooling layer 1
+    pool1 = K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv1)
+
+    # Convolutional layer 2
+    conv2 = K.layers.Conv2D(filters=16, kernel_size=(
+        5, 5), padding='valid', activation='relu',
+        kernel_initializer='he_normal')(pool1)
+
+    # Max pooling layer 2
+    pool2 = K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv2)
+
+    # Flatten the output
+    flat = K.layers.Flatten()(pool2)
+
+    # Fully connected layer 1
+    fc1 = K.layers.Dense(units=120, activation='relu',
+                         kernel_initializer='he_normal')(flat)
+
+    # Fully connected layer 2
+    fc2 = K.layers.Dense(units=84, activation='relu',
+                         kernel_initializer='he_normal')(fc1)
+
+    # Output softmax layer
+    output = K.layers.Dense(units=10, activation='softmax',
+                            kernel_initializer='he_normal')(fc2)
+
+    # Create and compile the model
+    model = K.Model(inputs=X, outputs=output)
+    model.compile(optimizer='adam',
+                  loss='categorical_crossentropy', metrics=['accuracy'])
+
+    return model
