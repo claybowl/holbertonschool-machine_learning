e3e77aa647d8a928eb05c600f3dbc88cf569fe96
claybowl <claybowlproductions@gmail.com>
2023-04-20 16:37:43 -0500

    testing

--- supervised_learning/0x00-object_detection/1-yolo.py
+++ supervised_learning/0x00-object_detection/1-yolo.py
@@ -50,8 +50,8 @@ class Yolo:
                         bh = ph * np.exp(th)
                         bx /= grid_width
                         by /= grid_height
-                        bw /= self.model.input.shape[1]
-                        bh /= self.model.input.shape[2]
+                        bw /= self.model.input.shape[1].value
+                        bh /= self.model.input.shape[2].value
                         x1 = (bx - (bw / 2)) * image_width
                         y1 = (by - (bh / 2)) * image_height
                         x2 = (bx + (bw / 2)) * image_width
