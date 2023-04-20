a06d7791eff5b7b7bb9090dd0221629635cb0c8c
claybowl <claybowlproductions@gmail.com>
2023-04-20 17:45:52 -0500

    task 2 attempt

---
+++ supervised_learning/0x00-object_detection/2-yolo.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+"""module 2-yolo.py
+"""
+import numpy as np
+from keras.models import load_mode
+
+
+class Yolo:
+    """Uses the Yolo v3 algorithm to perform object detection"""
+
+    def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
+        """Class constructor"""
+        self.model = load_model(model_path)
+        self.class_names = self._load_classes(classes_path)
+        self.class_t = class_t
+        self.nms_t = nms_t
+        self.anchors = anchors
+
+    def _load_classes(self, classes_path):
+        """Loads the classes from a file"""
+        with open(classes_path, 'r') as f:
+            class_names = f.readlines()
+        class_names = [c.strip() for c in class_names]
+        return class_names
+
+    def process_outputs(self, outputs, image_size):
+        boxes = []
+        box_confidences = []
+        box_class_probs = []
+        for i in range(len(outputs)):
+            boxes.append(outputs[i][..., :4])
+            box_confidences.append(1 / (1 + np.exp(-outputs[i][..., 4:5])))
+            box_class_probs.append(1 / (1 + np.exp(-outputs[i][..., 5:])))
+        image_height, image_width = image_size
+        for i in range(len(boxes)):
+            grid_width = outputs[i].shape[1]
+            grid_height = outputs[i].shape[0]
+            anchor_boxes = outputs[i].shape[2]
+            for cy in range(grid_height):
+                for cx in range(grid_width):
+                    for b in range(anchor_boxes):
+                        tx, ty, tw, th = boxes[i][cy, cx, b]
+                        pw, ph = self.anchors[i][b]
+                        bx = (1 / (1 + np.exp(-tx))) + cx
+                        by = (1 / (1 + np.exp(-ty))) + cy
+                        bw = pw * np.exp(tw)
+                        bh = ph * np.exp(th)
+                        bx /= grid_width
+                        by /= grid_height
+                        bw /= self.model.input.shape[1]
+                        bh /= self.model.input.shape[2]
+                        x1 = (bx - (bw / 2)) * image_width
+                        y1 = (by - (bh / 2)) * image_height
+                        x2 = (bx + (bw / 2)) * image_width
+                        y2 = (by + (bh / 2)) * image_height
+                        boxes[i][cy, cx, b] = [x1, y1, x2, y2]
+        return (boxes, box_confidences, box_class_probs)
+
+    def filter_boxes(self, boxes, box_confidences, box_class_probs):
+        filtered_boxes, box_classes, box_scores = None, [], []
+        for i in range(len(boxes)):
+            box_scores.append(box_confidences[i] * box_class_probs[i])
+
+        box_scores_list = [score.reshape(-1) for score in box_scores]
+        box_scores_all = np.concatenate(box_scores_list)
+
+        box_classes_all = np.argmax(np.concatenate(box_scores), axis=-1)
+        box_mask = box_scores_all >= self.class_t
+
+        filtered_boxes = np.concatenate(
+            [box.reshape(-1, 4) for box in boxes], axis=0)
+        filtered_boxes = filtered_boxes[box_mask]
+
+        box_classes = box_classes_all[box_mask]
+        box_scores = box_scores_all[box_mask]
+
+        return filtered_boxes, box_classes, box_scores
