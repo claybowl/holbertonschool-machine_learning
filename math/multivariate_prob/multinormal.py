b60a6f9da303f565fde8d5d60968474aa8fd1c3b
Clayton Christian <103296303+claybowl@users.noreply.github.com>
2023-05-18 03:17:55 +0000

    pdf update

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -20,3 +20,22 @@ class MultiNormal:
 
         deviation = data - self.mean
         self.cov = np.matmul(deviation, deviation.T) / (n - 1)
+
+    def pdf(self, x):
+        """Calculates the PDF at a data point"""
+        if not isinstance(x, np.ndarray):
+            raise TypeError("x must be a numpy.ndarray")
+
+        d = self.mean.shape[0]
+
+        if len(x.shape) != 2 or x.shape != (d, 1):
+            raise ValueError('x must have the shape ({}, 1)'.format(d))
+
+        det = np.linalg.det(self.cov)
+        inv = np.linalg.inv(self.cov)
+
+        denom = np.sqrt(((2 * np.pi) ** d) * det)
+        exponent = np.exp(-0.5 * np.dot(np.dot((x - self.mean).T, inv),
+                                        (x - self.mean)))
+
+        return float(exponent / denom)
