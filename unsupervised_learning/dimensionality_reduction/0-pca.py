badb335d7ce79854ca1cd0c17ef1a90daedc227d
claybowl <claybowlproductions@gmail.com>
2023-05-30 05:10:54 -0500

    added comments

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -7,7 +7,11 @@ import numpy as np
 
 def pca(X, var=0.95):
     """Function that performs PCA on a dataset"""
+    # Compute the SVD of X
     _, _, V = np.linalg.svd(X)
+    # Compute the variance explained by each component
     cumulative = np.cumsum(V**2) / np.sum(V**2)
+    # Find the number of principal components that explain 'var' of the variance
     r = (np.argwhere(cumulative >= var))[0, 0]
+    # Return the weights matrix
     return V.T[:, :r + 1]
