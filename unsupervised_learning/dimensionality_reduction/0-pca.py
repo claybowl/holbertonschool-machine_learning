4911657da9b5952206915b028d17bc572bbe84db
claybowl <claybowlproductions@gmail.com>
2023-05-30 06:48:11 -0500

    task 0 retry

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -8,7 +8,7 @@ import numpy as np
 def pca(X, var=0.95):
     """Function that performs PCA on a dataset"""
     # Compute the SVD of X
-    _, S, Vt = np.linalg.svd(X, full_matrices=False
+    _, S, Vt = np.linalg.svd(X, full_matrices=False)
     # Compute the variance explained by each component
     evr = S**2 / np.sum(S**2)
     # Calculate the cumsum
