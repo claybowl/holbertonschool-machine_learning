7358582f33971931c8d99a22c486ebd683fe2c73
claybowl <claybowlproductions@gmail.com>
2023-05-31 23:11:49 -0500

    pycode

--- unsupervised_learning/clustering/4-initialize.py
+++ unsupervised_learning/clustering/4-initialize.py
@@ -16,9 +16,14 @@ def initialize(X, k):
 
     Returns:
     tuple: (pi, m, S)
-        pi is a numpy.ndarray of shape (k,) containing the priors for each cluster, initialized evenly.
-        m is a numpy.ndarray of shape (k, d) containing the centroid means for each cluster, initialized with K-means.
-        S is a numpy.ndarray of shape (k, d, d) containing the covariance matrices for each cluster, initialized as identity matrices.
+        pi is a numpy.ndarray of shape (k,) containing
+        the priors for each cluster, initialized evenly.
+        m is a numpy.ndarray of shape (k, d) containing
+        the centroid means for each cluster,
+        initialized with K-means.
+        S is a numpy.ndarray of shape (k, d, d) containing
+        the covariance matrices for each
+        cluster, initialized as identity matrices.
     """
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None, None
