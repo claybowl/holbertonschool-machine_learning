7f50d64064db4e4e960a0d7ac0ae798837182eca
claybowl <claybowlproductions@gmail.com>
2023-06-02 22:36:48 -0500

    task 8 rework attempt

--- unsupervised_learning/clustering/8-EM.py
+++ unsupervised_learning/clustering/8-EM.py
@@ -10,26 +10,61 @@ maximization = __import__('7-maximization').maximization
 
 def expectation_maximization(X, k, iterations=1000,
                              tol=1e-5, verbose=False):
-    """performs the maximization"""
+    """
+    Performs the expectation maximization for a GMM.
 
-    # Initialize variables
+    Parameters:
+    X (numpy.ndarray): The data set. Shape (n, d).
+    k (int): The number of clusters.
+    iterations (int): The maximum number of
+    iterations for the algorithm.
+    tol (float): The tolerance of the log likelihood.
+    verbose (bool): Whether to print information
+    about the algorithm.
+
+    Returns:
+    tuple: The final values of pi, m, S, g, and ll.
+    """
+    # Validate inputs
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None, None, None, None, None
+    if not isinstance(k, int) or k <= 0:
+        return None, None, None, None, None
+    if not isinstance(iterations, int) or iterations <= 0:
+        return None, None, None, None, None
+    if not isinstance(tol, float) or tol < 0:
+        return None, None, None, None, None
+    if not isinstance(verbose, bool):
+        return None, None, None, None, None
+
+    # Initialize pi, m, and S
     pi, m, S = initialize(X, k)
-    g, _ = expectation(X, pi, m, S)
-    l_prev = 0
 
-    # Perform EM algorithm
-    for i in range(iterations):
-        pi, m, S = maximization(X, g)
-        g, l = expectation(X, pi, m, S)
+    # Initialize log likelihood
+    log_likelihood = [0, 0]
 
-        # Check for verbose
-        if verbose and (i % 10 == 0 or i == iterations - 1):
-            print('Log Likelihood after {} iterations:
-                  {}'.format(i, round(l, 5)))
+    # Perform expectation maximization
+    for i in range(iterations + 1):
+        # Expectation step
+        g, log_likelihood[1] = expectation(X, pi, m, S)
 
-        # Check for early stopping
-        if abs(l - l_prev) <= tol:
+        # Print log likelihood every 10 iterations if verbose is True
+        if verbose and i % 10 == 0:
+            print("Log Likelihood after {} iterations: {}"
+                  .format(i, log_likelihood[1].round(5)))
+
+        # Check for convergence
+        if abs(log_likelihood[0] - log_likelihood[1]) <= tol:
+            if verbose:
+                print("Log Likelihood after {} iterations: {}"
+                      .format(i, log_likelihood[1].round(5)))
             break
-        l_prev = l
 
-    return pi, m, S, g, l
+        # Maximization step
+        if i < iterations:
+            pi, m, S = maximization(X, g)
+
+        # Update log likelihood
+        log_likelihood[0] = log_likelihood[1]
+
+    return pi, m, S, g, log_likelihood[1]
