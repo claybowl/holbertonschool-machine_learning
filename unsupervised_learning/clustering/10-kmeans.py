daec63810a84cbf631194dc793ff031c2180eb9a
claybowl <claybowlproductions@gmail.com>
2023-06-02 23:38:38 -0500

    fixed import for sklearn

--- unsupervised_learning/clustering/10-kmeans.py
+++ unsupervised_learning/clustering/10-kmeans.py
@@ -6,12 +6,24 @@ import sklearn.cluster
 
 
 def kmeans(X, k):
-    """Performs K-me"""
+    """
+    Performs K-means clustering on the given data.
+
+    Parameters:
+    X (numpy.ndarray): The data to be clustered. Shape (n, d), where n is the number of data points and d is the number of dimensions.
+    k (int): The number of clusters.
+
+    Returns:
+    tuple: A tuple containing the cluster centroids and the labels for each data point.
+    """
     # Perform K-means clustering
-    kmeans = KMeans(n_clusters=k).fit(X)
+    kmeans_model = sklearn.cluster.KMeans(n_clusters=k)
+
+    # Fit the model to the data
+    kmeans_model.fit(X)
 
     # Get cluster centroids and labels
-    C = kmeans.cluster_centers_
-    clss = kmeans.labels_
+    centroids = kmeans_model.cluster_centers_
+    labels = kmeans_model.labels_
 
-    return C, clss
+    return centroids, labels
