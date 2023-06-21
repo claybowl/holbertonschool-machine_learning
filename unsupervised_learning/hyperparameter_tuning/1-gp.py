a94f50000224e79b5a98d7806b0e6654066ec60d
Clayton Christian <103296303+claybowl@users.noreply.github.com>
2023-06-21 17:39:19 +0000

    task 1 testing...

--- unsupervised_learning/hyperparameter_tuning/1-gp.py
+++ unsupervised_learning/hyperparameter_tuning/1-gp.py
@@ -28,12 +28,6 @@ class GaussianProcess:
     def kernel(self, X1, X2):
         """Calculates the covariance kernel matrix between
         two matrices using the RBF kernel."""
-        if X1.ndim == 1:
-            X1 = X1[:, None]
-        if X2.ndim == 1:
-            X2 = X2[:, None]
-        assert X1.shape[1] == X2.shape[1]
-
         sqdist = np.sum(X1**2, 1).reshape(-1, 1) +\
             np.sum(X2**2, 1) - 2 * np.dot(X1, X2.T)
         return self.sigma_f**2 * np.exp(-0.5 / self.l**2 * sqdist)
