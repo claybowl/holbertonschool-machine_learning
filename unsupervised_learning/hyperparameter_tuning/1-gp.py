6c943210271ca1b22334b2cfceae5b62afa1cd93
claybowl <claybowlproductions@gmail.com>
2023-06-21 08:36:45 -0500

    task 1 attempt

---
+++ unsupervised_learning/hyperparameter_tuning/1-gp.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""module 1-gp.py
+Creates class GaussianProcess that represents a
+noiseless 1D Gaussian process
+"""
+import numpy as np
+
+
+class GaussianProcess:
+    """Represents a noiseless 1D Gaussian process"""
+
+    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
+        """
+        X_init: numpy.ndarray of shape (t, 1)
+        representing the inputs already sampled
+        Y_init: numpy.ndarray of shape (t, 1)
+        representing the outputs for each input in X_init
+        l: length parameter for the kernel
+        sigma_f: standard deviation given to
+        the output of the black-box function
+        """
+        self.X = X_init
+        self.Y = Y_init
+        self.l = l
+        self.sigma_f = sigma_f
+        self.K = self.kernel(X_init, X_init)
+
+    def kernel(self, X1, X2):
+        """Calculates the covariance kernel matrix between
+        two matrices using the RBF kernel."""
+        sqdist = np.sum(X1**2, 1).reshape(-1, 1) + \
+            np.sum(X2**2, 1) - 2 * np.dot(X1, X2.T)
+        return self.sigma_f**2 * np.exp(-0.5 / self.l**2 * sqdist)
+
+    def predict(self, X_s):
+        """
+        Predict the mean and standard deviation of samples at X_s.
+        X_s is a numpy.ndarray of shape (s, 1) containing all the points whose
+        mean and standard deviation should be calculated
+        s is the number of sample points
+        """
+        K_s = self.kernel(self.X, X_s)
+        K_ss = self.kernel(X_s, X_s)
+        K_inv = np.linalg.inv(self.K)
+
+        # Mean
+        mu_s = K_s.T.dot(K_inv).dot(self.Y)
+
+        # Covariance
+        cov_s = K_ss - K_s.T.dot(K_inv).dot(K_s)
+
+        return mu_s, cov_s
