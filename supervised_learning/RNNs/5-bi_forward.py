410c0d58ab3e8e2110b5e05a645b954209ae9471
claybowl <claybowlproductions@gmail.com>
2023-07-19 22:56:46 -0500

    task 5 attempt

---
+++ supervised_learning/RNNs/5-bi_forward.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+"""module 0-rnn_cell
+contains the class RNNCell
+"""
+import numpy as np
+
+
+class BidirectionalCell:
+    """
+    Class that represents a bidirectional cell for a bidirectional RNN.
+    """
+
+    def __init__(self, i, h, o):
+        """
+        Initialize the cell.
+
+        Parameters:
+        i: integer, representing the dimensionality of the data
+        h: integer, representing the dimensionality of the hidden state
+        o: integer, representing the dimensionality of the outputs
+        """
+        # Weight matrices
+        self.Whf = np.random.normal(size=(i + h, h))  # Forward hidden state
+        self.Whb = np.random.normal(size=(i + h, h))  # Backward hidden state
+        self.Wy = np.random.normal(size=(2 * h, o))  # Output
+
+        # Bias vectors
+        self.bhf = np.zeros((1, h))  # Forward hidden state
+        self.bhb = np.zeros((1, h))  # Backward hidden state
+        self.by = np.zeros((1, o))  # Output
+
+    def softmax(self, x):
+        """
+        Compute softmax values for each sets of scores in x.
+
+        Parameters:
+        x: A numpy array which we want to apply softmax to.
+
+        Returns:
+        A numpy array representing softmax of input x.
+        """
+        e_x = np.exp(
+            x - np.max(x))  # Subtract max(x) to compute in a numerically stable way
+        return e_x / e_x.sum(axis=1, keepdims=True)
+
+    def forward(self, h_prev, x_t):
+        """
+        Perform forward propagation for one time step.
+
+        Parameters:
+        h_prev: numpy array of shape (m, h), contains the previous hidden state
+        x_t: numpy array of shape (m, i), contains the data input for the cell
+
+        Returns:
+        h_next: next hidden state
+        """
+        # Concatenate h_prev and x_t to match dimensions for matrix multiplication
+        concat = np.concatenate((h_prev, x_t), axis=1)
+
+        # Compute next hidden state using the formula: tanh(W . concat + b)
+        h_next = np.tanh(np.dot(concat, self.Whf) + self.bhf)
+
+        return h_next
---
+++ supervised_learning/RNNs/8-bi_rnn.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+"""module 0-rnn_cell
+contains the class RNNCell
+"""
+import numpy as np
