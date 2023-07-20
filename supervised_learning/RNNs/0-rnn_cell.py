1aaa18ea8d1a7c31243a7e3b2e01995c94724463
claybowl <claybowlproductions@gmail.com>
2023-07-19 20:56:32 -0500

    documentation

--- supervised_learning/RNNs/0-rnn_cell.py
+++ supervised_learning/RNNs/0-rnn_cell.py
@@ -7,16 +7,19 @@ import numpy as np
 
 class RNNCell:
     def __init__(self, i, h, o):
+        """Constructor"""
         self.Wh = np.random.normal(size=(i + h, h))
         self.Wy = np.random.normal(size=(h, o))
         self.bh = np.zeros((1, h))
         self.by = np.zeros((1, o))
 
     def softmax(self, x):
+        """softmax function"""
         e_x = np.exp(x - np.max(x))
         return e_x / e_x.sum(axis=1, keepdims=True)
 
     def forward(self, h_prev, x_t):
+        """Function that performs forward propagation for one time step"""
         concat = np.concatenate((h_prev, x_t), axis=1)
         h_next = np.tanh(np.dot(concat, self.Wh) + self.bh)
         y = self.softmax(np.dot(h_next, self.Wy) + self.by)
