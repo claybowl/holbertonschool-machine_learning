ea760217379839dc7a04d1f3a63f4073c014ec9f
claybowl <claybowlproductions@gmail.com>
2023-07-19 22:09:11 -0500

    task 3 attempt

--- supervised_learning/RNNs/3-lstm_cell.py
+++ supervised_learning/RNNs/3-lstm_cell.py
@@ -5,18 +5,20 @@ contains the class RNNCell
 import numpy as np
 
 
-class GRUCell:
+class LSTMCell:
     """class RNNCell"""
 
     def __init__(self, i, h, o):
-        """Constructor"""
-        self.Wz = np.random.normal(size=(i + h, h))
-        self.Wr = np.random.normal(size=(i + h, h))
-        self.Wh = np.random.normal(size=(i + h, h))
+        self.Wf = np.random.normal(size=(i + h, h))
+        self.Wu = np.random.normal(size=(i + h, h))
+        self.Wc = np.random.normal(size=(i + h, h))
+        self.Wo = np.random.normal(size=(i + h, h))
         self.Wy = np.random.normal(size=(h, o))
-        self.bz = np.zeros((1, h))
-        self.br = np.zeros((1, h))
-        self.bh = np.zeros((1, h))
+        self.bf = np.zeros((1, h))
+        self.bu = np.zeros((1, h))
+        self.bc = np.zeros((1, h))
+        self.bo = np.zeros((1, h))
+        self.by = np.zeros((1, o))
         self.by = np.zeros((1, o))
 
     def softmax(self, x):
@@ -30,12 +32,21 @@ class GRUCell:
 
     def forward(self, h_prev, x_t):
         """Function that performs forward propagation for one time step"""
+        # h_prev is a numpy.ndarray of shape (m, h) containing the previous
+        #
         concat = np.concatenate((h_prev, x_t), axis=1)
+        # forget gate
         f_t = self.sigmoid(np.dot(concat, self.Wf) + self.bf)
+        # update gate
         u_t = self.sigmoid(np.dot(concat, self.Wu) + self.bu)
+        # intermediate cell state
         c_intermediate = np.tanh(np.dot(concat, self.Wc) + self.bc)
+        # cell state
         c_next = f_t * c_prev + u_t * c_intermediate
+        # output gate
         o_t = self.sigmoid(np.dot(concat, self.Wo) + self.bo)
+        # hidden state
         h_next = o_t * np.tanh(c_next)
         y = self.softmax(np.dot(h_next, self.Wy) + self.by)
+
         return h_next, c_next, y
