d221e437386c13321373c5892b0d0b480b68fb38
claybowl <claybowlproductions@gmail.com>
2023-07-26 06:02:29 -0500

    btc prediction model

---
+++ supervised_learning/time_series/forecast_btc.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+"""module forecast_btc
+Forecasts the price of Bitcoin using a LSTM model
+"""
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
+import tensorflow as tf
+from sklearn.preprocessing import StandardScaler
+
+
+
+def create_dataset(dataset, look_back=1):
+    X, Y = [], []
+    for i in range(len(dataset)-look_back-1):
+        a = dataset[i:(i+look_back), 0]
+        X.append(a)
+        Y.append(dataset[i + look_back, 0])
+    return np.array(X), np.array(Y)
+
+look_back = 24
+X_train, Y_train = create_dataset(train, look_back)
+X_test, Y_test = create_dataset(test, look_back)
+
+# Reshape input to be [samples, time steps, features]
+X_train = np.reshape(X_train, (X_train.shape[0], 1, X_train.shape[1]))
+X_test = np.reshape(X_test, (X_test.shape[0], 1, X_test.shape[1]))
+
+# Split the data into training and testing sets
+train_size = int(len(data) * 0.8)
+test_size = len(data) - train_size
+train, test = data[0:train_size,:], data[train_size:len(data),:]
+
+
+# Initialize the LSTM model
+model = Sequential()
+
+# Add the first LSTM layer
+model.add(LSTM(units=50, return_sequences=True, input_shape=(X_train.shape[1], 1)))
+model.add(Dropout(0.2))
+
+# Add the second LSTM layer
+model.add(LSTM(units=50, return_sequences=True))
+model.add(Dropout(0.2))
+
+# Add the third LSTM layer
+model.add(LSTM(units=50, return_sequences=True))
+model.add(Dropout(0.2))
+
+# Add the fourth LSTM layer
+model.add(LSTM(units=50))
+model.add(Dropout(0.2))
+
+# Add the output layer
+model.add(Dense(units=1))
+
+# Compile the model
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+# Print the model summary
+model.summary()
+
+# Plotting the results
+plt.figure(figsize=(16,8))
+plt.plot(closing_price, label='Predicted Close Price')
+plt.title('BTC Price Prediction')
+plt.xlabel('Time')
+plt.ylabel('BTC Price')
+plt.legend(loc='best')
+plt.show()
