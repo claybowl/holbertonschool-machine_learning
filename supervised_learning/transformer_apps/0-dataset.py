62b0870917bc07b2e7ecccbca449d592ce76ff1b
claybowl <claybowlproductions@gmail.com>
2023-08-20 13:34:30 -0500

    made some changes to tokenize_dataset function

--- supervised_learning/transformer_apps/0-dataset.py
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -4,6 +4,7 @@ Create the class Dataset that
 loads and preps a
 dataset for machine translation
 """
+import tensorflow_datasets as tfds
 import tensorflow as tf
 
 
@@ -11,16 +12,15 @@ class Dataset:
     """class Dataset"""
 
     def __init__(self):
-        self.data_train = tf.data.Dataset.from_tensor_slices(tf.config.experimental.get_memory_info(tf.data.Dataset.train_split_slices('ted_hrlr_translate/pt_to_en')))
-        self.data_valid = tf.data.Dataset.from_tensor_slices(tf.config.experimental.get_memory_info(tf.data.Dataset.validate_split_slices('ted_hrlr_translate/pt_to_en')))
-        self.tokenizer_pt = tf.keras.preprocessing.text.Tokenizer(num_words=2**15)
-        self.tokenizer_en = tf.keras.preprocessing.text.Tokenizer(num_words=2**15)
+        """Construct"""
+        self.data_train = tfds.load('ted_hrlr_translate/pt_to_en', split='train', as_supervised=True)
+        self.data_valid = tfds.load('ted_hrlr_translate/pt_to_en', split='validation', as_supervised=True)
+        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(self.data_train)
 
     def tokenize_dataset(self, data):
-        pt = data.pt
-        en = data.en
-
-        tokenizer_pt.add_tokens(pt.numpy())
-        tokenizer_en.add_tokens(en.numpy())
-
-        return self.tokenizer_pt, self.tokenizer_en
+        """tokenize_dataset"""
+        tokenizer_pt = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(
+            (pt.numpy() for pt, en in data), target_vocab_size=2**15)
+        tokenizer_en = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(
+            (en.numpy() for pt, en in data), target_vocab_size=2**15)
+        return tokenizer_pt, tokenizer_en
