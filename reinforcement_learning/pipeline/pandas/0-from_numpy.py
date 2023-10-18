ff38608d86b24e22915e070740588b529167028d
Clayton Christian <claybowlproductions@gmail.com>
2023-10-17 21:22:43 -0500

    begin coding

---
+++ reinforcement_learning/pipeline/pandas/0-from_numpy.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""module 0-from_numpy
+converts a numpy.ndarray to a pd.DataFrame
+"""
+import pandas as pd
+
+
+def from_numpy(array):
+    """function that converts numpy array into pd.DataFrame"""
+    
