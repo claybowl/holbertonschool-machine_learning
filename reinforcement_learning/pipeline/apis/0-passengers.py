a2511b66123f25838d6dc26c689279bafae42f7d
Clayton Christian <claybowlproductions@gmail.com>
2023-10-24 19:01:54 -0500

    task 0 api push

--- reinforcement_learning/pipeline/apis/0-passengers.py
+++ reinforcement_learning/pipeline/apis/0-passengers.py
@@ -4,16 +4,27 @@ Fetches a list of ships from the API
 """
 import requests
 
-def availableShips(passengerCount, page=1):
-    url = f"https://swapi.dev/api/ships/?page={page}"
-
-    response = requests.get(url)
-    ships = response.json()['results']
-
+def availableShips(passengerCount):
+    """Returns a list of ships available for a given
+    passenger count"""
+    # empty list to hold names of available ships
     available_ships = []
 
-    for ship in ships:
-        if ship['passengers'] >= passengerCount:
-            available_ships.append(ship)
+    # initialize URL for Swapi API endpoint for starships
+    url = "https://swapi.dev/api/starships/"
 
+    while url:
+        # connect with API
+        response = requests.get(url)
+        ship_data = response.json()
+
+        # loop through starship pages
+        for starship in ship_data['results']:
+            try:
+                capacity = int(starship['passengers'])
+            except ValueError:
+                continue
+            if capacity >= passengerCount:
+                available_ships.append(starship['name'])
+        url = ship_data['next']
     return available_ships
