aa173c67d0ab68a1bf9b2dd5c27e8e278a1e10a6
Clayton Christian <claybowlproductions@gmail.com>
2023-10-26 13:13:29 -0500

    update

--- pipeline/apis/3-first_launch.py
+++ pipeline/apis/3-first_launch.py
@@ -11,10 +11,11 @@ L_URL = "https://api.spacexdata.com/v4/launchpads/"
 R_URL = "https://api.spacexdata.com/v4/rockets/"
 LA_URL = "https://api.spacexdata.com/v4/launches"
 
+
 def fetch_first_launch():
     """Return the first launch"""
     # Fetch launches from SpaceX API
-    response = requests.get("LA_URL")
+    response = requests.get(LA_URL)
     launches = response.json()
 
     # Sort launches by date_unix
@@ -30,17 +31,18 @@ def fetch_first_launch():
     launchpad_id = first_launch['launchpad']
 
     # Fetch rocket details
-    rocket_response = requests.get("R_URL")
-
+    rocket_response = requests.get(R_URL + rocket_id)
     rocket_name = rocket_response.json()['name']
 
     # Fetch launchpad details
-    launchpad_response = requests.get("L_URL" + launchpad_id)
+    launchpad_response = requests.get(L_URL + launchpad_id)
     launchpad_data = launchpad_response.json()
     launchpad_name = launchpad_data['name']
     launchpad_locality = launchpad_data['locality']
 
     # Print the information
     # Using join()
-    print(" ".join([launch_name, "(", launch_date, rocket_name, ")", "-",
-                launchpad_name, "(", launchpad_locality, ")"]))
+    print(" ".join([
+        launch_name, "(", launch_date, rocket_name, ")", "-",
+        launchpad_name, "(", launchpad_locality, ")"
+    ]))
