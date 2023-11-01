6d5dc741a561600b322d38f4c53d6b1bb26a7fc6
claybowl <claybowlproductions@gmail.com>
2023-11-01 23:26:20 +0000

    task 17

---
+++ pipeline/databases/17-store.sql
@@ -0,0 +1,13 @@
+-- trigger that decreases the quantity of an item after adding a new order.
+--
+DELIMITER //
+CREATE TRIGGER decrease_quantity
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
+END;
+//
+DELIMITER ;
