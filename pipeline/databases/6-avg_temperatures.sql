296ac987a48a0722fe2bfd243e299cc2923b0dc5
Clayton Christian <claybowlproductions@gmail.com>
2023-11-01 14:20:08 -0500

    task 6 update

--- pipeline/databases/6-avg_temperatures.sql
+++ pipeline/databases/6-avg_temperatures.sql
@@ -1,6 +1,5 @@
 -- Calculate the average temperature by city
 --
-SELECT city, AVG(temperature) AS avg_temp
-FROM temperatures  -- Assuming the table name is 'temperatures'
+SELECT city, AVG(value) AS avg_temp FROM temperatures
 GROUP BY city
 ORDER BY avg_temp DESC;
