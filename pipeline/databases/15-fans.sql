83d6f0c8469c3a61de849aa8e591f9482d2af6b4
claybowl <claybowlproductions@gmail.com>
2023-11-01 23:02:30 +0000

    task 15

--- pipeline/databases/15-fans.sql
+++ pipeline/databases/15-fans.sql
@@ -1,6 +1,5 @@
 -- ranks country origins of bands, ordered by number of non-unique fans
 --
-SELECT origin, SUM(nb_fans) AS nb_fans
-FROM metal_bands
+SELECT origin, SUM(fans) AS nb_fans FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
