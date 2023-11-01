ad654f624f8c1401f4bff18e7c52431cb76c7033
claybowl <claybowlproductions@gmail.com>
2023-11-01 23:20:40 +0000

    task 16

--- pipeline/databases/16-glam_rock.sql
+++ pipeline/databases/16-glam_rock.sql
@@ -1,10 +1,6 @@
 -- list all bands with glam rock as their main style
 --
-SELECT band_name, 
-       CASE 
-           WHEN split IS NOT NULL THEN split - formed
-           ELSE 2020 - formed
-       END AS 'lifespan until 2020 (in years)'
+SELECT band_name, IFNULL((split-formed), (2020 - formed)) AS lifespan
 FROM metal_bands
-WHERE main_style = 'Glam rock'
-ORDER BY `lifespan until 2020 (in years)` DESC;
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
