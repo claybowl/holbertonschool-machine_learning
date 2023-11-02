18e085a9e7ce862bee847e5f6ac8e52dc81dac4e
claybowl <claybowlproductions@gmail.com>
2023-11-01 21:07:32 -0700

    task 34

--- pipeline/databases/34-log_stats.py
+++ pipeline/databases/34-log_stats.py
@@ -9,19 +9,25 @@ if __name__ == "__main__":
     # Connect to the MongoDB client
     client = MongoClient('mongodb://127.0.0.1:27017')
     # Access the 'logs' database and 'nginx' collection
-    nginx_collection = client.logs.nginx
+    db = client['logs']
+    nginx_collection = db['nginx']
 
     # Count the total number of logs
-    total_logs = nginx_collection.count_documents({})
-    print("{} logs".format(total_logs))
+    nginx_logs = nginx_collection.count_documents({})
+    print(str(nginx_logs) + ' logs')
+    print("Methods:")
 
     # Print the methods statistics
-    print("Methods:")
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for method in methods:
-        count = nginx_collection.count_documents({"method": method})
-        print("\tmethod {}: {}".format(method, count))
+    method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    method_stats_dict = {}
 
-    # Count the number of documents with method=GET and path=/status
-    status_check = nginx_collection.count_documents({"method": "GET", "path": "/status"})
-    print("{} status check".format(status_check))
+    for method in method:
+        method_count = nginx_collection.count_documents({"method": method})
+        method_stats_dict[method] = method_count
+
+    for item, value in method_stats_dict.items():
+        print('\tmethod ' + item + ': ' + str(value))
+
+    status_count = nginx_collection.count_documents({'method': 'GET',
+                                                     'path': "/status"})
+    print(str(status_count) + " status check")
