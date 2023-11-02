e56341a6d114e0a92cf924bae48e0221c7678fee
claybowl <claybowlproductions@gmail.com>
2023-11-01 20:35:11 -0700

    task 31

---
+++ pipeline/databases/31-insert_school.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""module 31-insert_school
+Inserts new python document in a collection
+based on kwargs.
+"""
+
+
+def insert_school(mongo_collection, kwargs):
+    """inserts a new document into collection"""
+    # Insert the document and get the inserted document's ID
+    inserted_doc = mongo_collection.insert_one(kwargs)
+    
+    # Return the new _id
+    return inserted_doc.inserted_id
