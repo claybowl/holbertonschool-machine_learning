bd73510f1d52e831c252ad05678c76eaca11177e
claybowl <claybowlproductions@gmail.com>
2023-11-02 00:09:55 +0000

    task 20 attempt

--- pipeline/databases/20-average_score.sql
+++ pipeline/databases/20-average_score.sql
@@ -1,15 +1,20 @@
 -- compute and store the average score
 --
-DELIMITER //
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+DELIMITER &&
+
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+
+CREATE PROCEDURE ComputeAverageScoreForUser (
+    IN user_id INT
+)
 BEGIN
-    DECLARE avg_score FLOAT;
+    DECLARE average_score DEC(10, 1);
 
-    -- Compute the average score for the given user_id
-    SELECT AVG(score) INTO avg_score FROM corrections WHERE user_id = user_id;
+    SELECT AVG(score) INTO average_score
+        FROM corrections
+        WHERE user_id = corrections.user_id;
+    
+    UPDATE users SET average_score = average_score WHERE id = user_id;
+END; &&
 
-    -- Update the average_score in the users table
-    UPDATE users SET average_score = avg_score WHERE id = user_id;
-END;
-//
 DELIMITER ;
